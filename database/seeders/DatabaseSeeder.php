b00a9eeba095c9f9378b1f94cb11e5f5150453a4
QuocTuanIT <kutuanonline199@gmail.com>
2023-12-03 16:05:21 +0700

    define DB, factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $users = \App\Models\User::factory(10)->create();
+
+        $posts = \App\Models\Post::factory(50)->recycle($users)->create();
+        // \App\Models\Post::factory(100)->create();
+        \App\Models\Category::factory(5)->create();
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Tuantq',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12341234'),
+        ]);
     }
 }
